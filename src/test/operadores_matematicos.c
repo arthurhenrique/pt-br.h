2c597453ab6f3ceb1822d1e4970c53eb177bec09
arthurhenrique <arthur393@gmail.com>
2016-06-06 11:19:28 -0300

    changed test

--- src/test/operadores_matematicos.c
+++ src/test/operadores_matematicos.c
@@ -3,7 +3,7 @@
 inicio
 	inteiro x=10,y=8;
 	escreva("Simples calculo com valores de x = %d e y = %d\n\n",x,y);
-	escreva("%d / %d = %d\n",x,y,x dividido por y);
+	escreva("%d / %d = %d\n",x,y,x dividido y);
 	escreva("%d * %d = %d\n",x,y,x vezes y);
 	escreva("%d + %d = %d\n",x,y,x mais y);
 	escreva("%d - %d = %d\n",x,y,x menos y);
