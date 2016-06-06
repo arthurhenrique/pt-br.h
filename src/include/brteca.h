df00df22ddc30e47de466f6302e17da3ef21fecd
Arthur Henrique <arthurhenrique@users.noreply.github.com>
2016-06-06 10:19:42 -0300

    Update brteca.h

--- src/include/brteca.h
+++ src/include/brteca.h
@@ -3,46 +3,48 @@
 
 #include <stdio.h>
 
-#define inicio      int main(int argc, char *argv[]){
-#define fim         }
+#define inicio       int main(int argc, char *argv[]){
+#define fim          }
 //Variáveis
-#define inteiro     int
-#define real        double
-#define letra       char
-#define logico      int
-#define verdade     1
-#define falso       0
+#define inteiro      int
+#define real         double
+#define letra        char
+#define logico       int
+#define verdade      1
+#define falso        0
 //I/O
-#define leia        scanf
-#define escreva     printf
+#define leia         scanf
+#define escreva      printf
 //Condicionais
-#define se          if
-#define entao       {
-#define senao       else
-#define fimse       }
-#define fimsenao    }
+#define se           if
+#define entao        {
+#define senao        else
+#define fim_se       }
+#define fim_senao    }
 //Laços de repetição
-#define faca        do
-#define enquanto    while
-#define fimenquanto }
-#define escolha     switch
-#define fimescolha  }
-#define caso        case
-#define pare        break;
-#define para        for
-#define fimpara     }
+#define faca         do
+#define enquanto     while
+#define fim_enquanto }
+#define escolha      switch
+#define fim_escolha  }
+#define caso         case
+#define pare         break;
+#define para         for
+#define fim_para     }
 //Operadores matemáticos
-#define dividido    /
-#define vezes       *
-#define mais        +
-#define menos       -
+#define dividido     /
+#define vezes        *
+#define mais         +
+#define menos        -
 //Operadores lógico
-#define maior       >
-#define menor       <
-#define igual       ==
-#define diferente   !
-#define e           &&
-#define ou          ||
-#define oux         ^^
+#define maior        >
+#define menor        <
+#define maior_igual  >=
+#define menor_igual  <=
+#define igual        ==
+#define diferente    !
+#define e            &&
+#define ou           ||
+#define oux          ^^
 
 #endif //_BRTECA_H_
